68d67508b49c488fbdbf93309a0b934f9e918e4e
Johannes 'josch' Schauer <josch@mister-muffin.de>
2017-01-20 05:21:27 +0100

    setup.py: bump python version to 3.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'Environment :: Console',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'License :: OSI Approved :: GNU Lesser General Public License v3 '
         '(LGPLv3)',
