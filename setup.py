a270c987f0f519c9ab451072311ae1ef387a9bcd
Johannes 'josch' Schauer <josch@mister-muffin.de>
2020-04-23 06:57:38 +0200

    setup.py: stop declaring support for Python2

--- setup.py
+++ setup.py
@@ -33,8 +33,6 @@ setup(
         'Intended Audience :: Other Audience',
         'Environment :: Console',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
